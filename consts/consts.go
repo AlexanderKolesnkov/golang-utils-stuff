f843aba6ec90444e3c8c48913374c2f45e68204d
rfh <diobrandomolodec@gmai.com>
2025-03-04 09:56:15 +0300

    add GetAllTimeframes in consts

--- consts/consts.go
+++ consts/consts.go
@@ -1,7 +1,18 @@
 package consts
 
 const (
-	Buy = "Buy"
-	Sell = "Sell"
+	Buy        = "Buy"
+	Sell       = "Sell"
 	TimeLayout = "2006-01-02 15:04:05"
 )
+
+// GetAllTimeframes
+//
+// returns "1", "5", "15", "30", "60", "240", "D", "W", "M"
+func GetAllTimeframes() []string {
+	return []string{"1", "5", "15", "30", "60", "240", "D", "W", "M"}
+}
+
+func SingleIndicatorHeader() []string {
+	return []string{"Time", "Value"}
+}
