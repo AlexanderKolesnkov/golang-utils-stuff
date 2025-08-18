a73ee18b39517fce1e2305ef00edb3f9ba2e67f5
AlexanderKolesnkov <diobrandomolodec@gmail.com>
2025-08-18 14:54:14 +0300

    make pkg timefh

---
+++ timefh/timeframes.go
@@ -0,0 +1,107 @@
+package timefh
+
+import (
+	"github.com/AlexanderKolesnkov/golang-utils-stuff/consts"
+	"time"
+)
+
+const (
+	Minute = 60
+	Hour   = Minute * 60
+	Day    = Hour * 24
+)
+
+func CalcTimeframesCount(timeframe, start, end string) (int, error) {
+	startTime, err := time.Parse(consts.TimeLayout, start)
+	if err != nil {
+		return 0, err
+	}
+	endTime, err := time.Parse(consts.TimeLayout, end)
+	if err != nil {
+		return 0, err
+	}
+
+	duration := endTime.Sub(startTime)
+
+	switch timeframe {
+	case "1":
+		return int(duration.Minutes()), nil
+	case "5":
+		return int(duration.Minutes()) / 5, nil
+	case "15":
+		return int(duration.Minutes()) / 15, nil
+	case "30":
+		return int(duration.Minutes()) / 30, nil
+	case "60":
+		return int(duration.Hours()), nil
+	case "240":
+		return int(duration.Hours()) / 4, nil
+	case "D":
+		return int(duration.Hours()) / 24, nil
+	}
+
+	return -1, nil
+}
+
+func IsUnixEnds(unix int, duration int, counter *int) bool {
+	switch duration {
+	case Day * 3:
+		if unix%86400 == 0 {
+			*counter++
+			if *counter == 3 {
+				*counter = 0
+				return true
+			}
+		}
+
+		return false
+	case Day:
+		if unix%86400 == 0 {
+			return true
+		}
+
+		return false
+	case Hour * 12:
+		if unix%86400 == 0 || unix%86400 == 43200 {
+			return true
+		}
+
+		return false
+
+	case Hour * 2:
+		if unix%3600 == 0 && (unix/3600)%2 == 0 {
+			return true
+		}
+
+		return false
+	case Hour:
+		if unix%3600 == 0 {
+			return true
+		}
+
+		return false
+	default:
+		return false
+	}
+}
+
+func ConvertTimeframeInUnix(timeframe string) int {
+	switch timeframe {
+	case "1":
+		return 60
+	case "5":
+		return 300
+	case "15":
+		return 900
+	case "30":
+		return 1800
+	case "60":
+		return 3600
+	case "240":
+		return 14400
+	case "D":
+		return 86400
+	default:
+		return -1
+	}
+}
--- utils/time.go
+++ utils/time.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"github.com/AlexanderKolesnkov/golang-utils-stuff/consts"
 	"time"
 )
 
@@ -33,99 +32,4 @@ func TimeDeflect(start, tf string, deflect int) (string, int, error) {
 
 func TimeNowFormat() string {
 	return time.Now().UTC().Format("2006-01-02 15:04:05")
-}
-
-func CalcTimeframesCount(timeframe, start, end string) (int, error) {
-	startTime, err := time.Parse(consts.TimeLayout, start)
-	if err != nil {
-		return 0, err
-	}
-	endTime, err := time.Parse(consts.TimeLayout, end)
-	if err != nil {
-		return 0, err
-	}
-
-	duration := endTime.Sub(startTime)
-
-	switch timeframe {
-	case "1":
-		return int(duration.Minutes()), nil
-	case "5":
-		return int(duration.Minutes()) / 5, nil
-	case "15":
-		return int(duration.Minutes()) / 15, nil
-	case "30":
-		return int(duration.Minutes()) / 30, nil
-	case "60":
-		return int(duration.Hours()), nil
-	case "240":
-		return int(duration.Hours()) / 4, nil
-	case "D":
-		return int(duration.Hours()) / 24, nil
-	}
-
-	return -1, nil
-}
-
-func IsUnixEnds(unix int, timeframe string, counter *int) bool {
-	switch timeframe {
-	case "3D":
-		if unix%86400 == 0 {
-			*counter++
-			if *counter == 3 {
-				*counter = 0
-				return true
-			}
-		}
-
-		return false
-	case "D":
-		if unix%86400 == 0 {
-			return true
-		}
-
-		return false
-	case "12h":
-		if unix%86400 == 0 || unix%86400 == 43200 {
-			return true
-		}
-
-		return false
-
-	case "2h":
-		if unix%3600 == 0 && (unix/3600)%2 == 0 {
-			return true
-		}
-
-		return false
-	case "1h":
-		if unix%3600 == 0 {
-			return true
-		}
-
-		return false
-	default:
-		return false
-	}
-}
-
-func ConvertTimeframeInUnix(timeframe string) int {
-	switch timeframe {
-	case "1":
-		return 60
-	case "5":
-		return 300
-	case "15":
-		return 900
-	case "30":
-		return 1800
-	case "60":
-		return 3600
-	case "240":
-		return 14400
-	case "D":
-		return 86400
-	default:
-		return -1
-	}
 }
